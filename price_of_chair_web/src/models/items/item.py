8a6d05019886e0b266c586b431da887a76d383e1
Alexa Sih <alexa17cassidy@gmail.com>
2018-08-15 15:36:16 -0700

    load item prices

--- price_of_chair_web/src/models/items/item.py
+++ price_of_chair_web/src/models/items/item.py
@@ -1,11 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
 __author__ = 'alexasih'
 
 
 class Item(object):
-    def __init__(self, name, price, url):
+    def __init__(self, name, price, url, store):
         self.name = name
-        self.price = price
         self.url = url
+        self.store = store
+        tag_name = store.tag_name
+        query = store.query
+        self.price = self.load_price(tag_name, query)
 
     def __repr(self):
         return "<Item {} with URL {}>".format(self.name, self.url)
+
+    def load_price(self, tag_name, query):
+        # Amazon: <span id="priceblock_ourprice" class="a-size-medium a-color-price">$3,099.00</span>
+        request = requests.get(self.url)
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        element = soup.find(tag_name, query)
+        string_price = element.text.strip()
+
+        pattern = re.compile("(\d+.\d+)")
+        match = pattern.search(string_price)
+
+        return match.group()
+
+
+
