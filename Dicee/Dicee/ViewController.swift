eb71601113e03c75be8fbb758fd7029a3db3ba2b
Alexa Sih <alexa17cassidy@gmail.com>
2022-07-06 23:39:36 -0700

    updated dicee 2022

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -1,59 +1,24 @@
 //
 //  ViewController.swift
-//  Dicee
+//  Dicee-iOS13
 //
-//  Created by laptop on 10/19/17.
-//  Copyright © 2017 Alexa Sih. All rights reserved.
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var randomDiceIndex1 : Int = 0
-    var randomDiceIndex2 : Int = 0
-    
-    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-        updateDiceImages()
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    let imageArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        //UInt = unsigned, can only be positive integer 32-bit number, saying how large the number can be
-        
-        updateDiceImages()
-        
-
+        diceImageView1.image = imageArray.randomElement()
+        diceImageView2.image = imageArray[Int.random(in: 0...5)]
     }
     
-    func updateDiceImages() {
-        
-        randomDiceIndex1 = Int(arc4random_uniform(6))
-        randomDiceIndex2 = Int(arc4random_uniform(6))
-        
-        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
-        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-    }
-    
-    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-
-        updateDiceImages()
-        
-    }
-
 }
-
 
